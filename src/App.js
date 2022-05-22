10dbebd8fc8086d35a6a503b62e4b394c02f5ea6
WolfHybrid <lequangtuan1605@gmail.com>
2022-05-22 21:06:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Web3 from "web3/dist/web3.min.js";
+import detectEthereumProvider from "@metamask/detect-provider";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  });
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+
+      if (provider) {
+        setWeb3Api({
+          provider,
+          web3: new Web3(provider),
+        });
+      } else {
+        console.error("please, install Metamask wallet");
+      }
+    };
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    web3Api.web3 && getAccount();
+  }, [web3Api]);
+
+  const connectWallet = () => {
+    web3Api.provider &&
+      web3Api.provider.request({
+        method: "eth_requestAccounts",
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          Current Balance: <strong>10</strong> ETH
+        </div>
+        <button className="button is-primary mr-2">Donate</button>
+        <button className="button is-danger mr-2">Withdraw</button>
+        <button className="button is-link" onClick={connectWallet}>
+          Connect Wallet
+        </button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Accounts Address: </strong>
+          {account ? account : "Accounts need to login"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
